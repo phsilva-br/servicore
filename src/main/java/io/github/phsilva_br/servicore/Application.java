6dab221b5ce33b85952285d56b9c30728b5028e6
Pablo Henrique <258694789+phsilva-br@users.noreply.github.com>
2026-02-23 22:41:26 -0300

    Initial commit

---
+++ src/main/java/io/github/phsilva_br/servicore/Application.java
@@ -0,0 +1,13 @@
+package io.github.phsilva_br.servicore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
